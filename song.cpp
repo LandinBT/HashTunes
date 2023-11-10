5a817f8b18e9dfd68d4264402e3d488f18f5d8c1
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2023-11-10 13:02:15 -0600

    Definition & implementation of the Song class.

---
+++ song.cpp
@@ -0,0 +1,99 @@
+#include "song.hpp"
+
+Song::Song() {}
+
+Song::Song(const Song& s) : id(s.id), title(s.title), artist(s.artist),
+    duration(s.duration) {}
+
+Song& Song::operator = (const Song& s) {
+    id = s.id;
+    title = s.title;
+    artist = s.artist;
+    duration = s.duration;
+
+    return *this;
+    }
+
+void Song::setId(const int& i) {
+    id = i;
+    }
+
+void Song::setTitle(const std::string& t) {
+    title = t;
+    }
+
+void Song::setArtist(const std::string& a) {
+    artist = a;
+    }
+
+void Song::setDuration(const Time& d) {
+    duration = d;
+    }
+
+std::string Song::toString() const {
+    return title + '\n' +
+           artist + '\n' +
+           duration.toString();
+    }
+
+int Song::getId() const {
+    return id;
+    }
+
+std::string Song::getTitle() const {
+    return title;
+    }
+
+std::string Song::getArtist() const {
+    return artist;
+    }
+
+Time Song::getDuration() const {
+    return duration;
+    }
+
+bool Song::operator == (const Song& s) const {
+    return id == s.id;
+    }
+
+bool Song::operator != (const Song& s) const {
+    return !(*this == s);
+    }
+
+bool Song::operator < (const Song& s) const {
+    return id < s.id;
+    }
+
+bool Song::operator <= (const Song& s) const {
+    return *this < s or *this == s;
+    }
+
+bool Song::operator > (const Song& s) const {
+    return !(*this <= s);
+    }
+
+bool Song::operator >= (const Song& s) const {
+    return !(*this < s);
+    }
+
+std::istream& operator >> (std::istream& is, Song& s) {
+    std::string str;
+
+    std::getline(is, str, '*');
+    s.id = stoi(str);
+
+    std::getline(is, s.title, '*');
+
+    std::getline(is, s.artist, '*');
+
+    is >> s.duration;
+
+    return is;
+    }
+
+std::ostream& operator << (std::ostream& os, const Song& s) {
+    return os << s.id << "*"
+           << s.title << "*"
+           << s.artist << "*"
+           << s.duration;
+    }
---
+++ song.hpp
@@ -0,0 +1,44 @@
+#ifndef SONG_HPP_INCLUDED
+#define SONG_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+#include "time.hpp"
+
+class Song {
+        int id;
+        std::string title;
+        std::string artist;
+        Time duration;
+
+    public:
+        Song();
+        Song(const Song&);
+
+        Song& operator = (const Song&);
+
+        void setId(const int&);
+        void setTitle(const std::string&);
+        void setArtist(const std::string&);
+        void setDuration(const Time&);
+
+        std::string toString() const;
+
+        int getId() const;
+        std::string getTitle() const;
+        std::string getArtist() const;
+        Time getDuration() const;
+
+        bool operator == (const Song&) const;
+        bool operator != (const Song&) const;
+        bool operator < (const Song&) const;
+        bool operator <= (const Song&) const;
+        bool operator > (const Song&) const;
+        bool operator >= (const Song&) const;
+
+        friend std::istream& operator >> (std::istream&, Song&);
+        friend std::ostream& operator << (std::ostream&, const Song&);
+    };
+
+#endif // SONG_HPP_INCLUDED
