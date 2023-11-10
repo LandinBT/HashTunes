420549be222289b2e31230e0b5e17f07f8d99481
LandinBT <daniel.basulto0859@alumnos.udg.mx>
2023-11-10 13:01:50 -0600

    Definition & implementation of the Time class.

---
+++ time.cpp
@@ -0,0 +1,84 @@
+#include "time.hpp"
+
+bool Time::isValid(const unsigned short& m, const unsigned short& s) const {
+    return m >= 0 and m < 100 and s >= 0 and s < 60;
+    }
+
+int Time::toInt() const {
+    return minute*100 + second;
+    }
+
+Time::Time() : minute(0), second(0) {}
+
+Time::Time(const Time& t) : minute(t.minute), second(t.second) {}
+
+Time& Time::operator = (const Time& t) {
+    minute = t.minute;
+    second = t.second;
+
+    return *this;
+    }
+
+void Time::setMinute(const unsigned short& m) {
+    if(isValid(m, second)) {
+        minute = m;
+    }
+    }
+
+void Time::setSecond(const unsigned short& s) {
+    if(isValid(minute, s)) {
+        second = s;
+    }
+    }
+
+std::string Time::toString() const {
+    return std::to_string(minute) + ":" + std::to_string(second);
+    }
+
+unsigned short Time::getMinute() const {
+    return minute;
+    }
+
+unsigned short Time::getSecond() const {
+    return second;
+    }
+
+bool Time::operator == (const Time& t) const {
+    return toInt() == t.toInt();
+    }
+
+bool Time::operator != (const Time& t) const {
+    return !(*this == t);
+    }
+
+bool Time::operator < (const Time& t) const {
+    return toInt() < t.toInt();
+    }
+
+bool Time::operator <= (const Time& t) const {
+    return *this < t or *this == t;
+    }
+
+bool Time::operator > (const Time& t) const {
+    return !(*this <= t);
+    }
+
+bool Time::operator >= (const Time& t) const {
+    return !(*this < t);
+    }
+
+std::istream& operator >> (std::istream& is, Time& t) {
+    std::string str;
+
+    std::getline(is, str, ':');
+    t.minute = stoi(str);
+
+    std::getline(is, str, '*');
+    t.second = stoi(str);
+
+    return is;
+    }
+
+std::ostream& operator << (std::ostream& os, const Time& t) {
+    return os << t.minute << ":" << t.second << "*";
+    }
---
+++ time.hpp
@@ -0,0 +1,40 @@
+#ifndef TIME_HPP_INCLUDED
+#define TIME_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+class Time {
+        unsigned short minute;
+        unsigned short second;
+
+        bool isValid(const unsigned short&, const unsigned short&) const;
+
+        int toInt() const;
+
+    public:
+        Time();
+        Time(const Time&);
+
+        Time& operator = (const Time&);
+
+        void setMinute(const unsigned short&);
+        void setSecond(const unsigned short&);
+
+        std::string toString() const;
+
+        unsigned short getMinute() const;
+        unsigned short getSecond() const;
+
+        bool operator == (const Time&) const;
+        bool operator != (const Time&) const;
+        bool operator < (const Time&) const;
+        bool operator <= (const Time&) const;
+        bool operator > (const Time&) const;
+        bool operator >= (const Time&) const;
+
+        friend std::istream& operator >> (std::istream&, Time&);
+        friend std::ostream& operator << (std::ostream&, const Time&);
+    };
+
+#endif // TIME_HPP_INCLUDED
